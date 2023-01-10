c0dc0ad7965f9297e587485939fc3886b1930a65
LazarIgnjatovic <ignjatoviclaza@gmail.com>
2023-01-10 12:39:26 +0100

    GUI

--- unify_csv/unify_csv.py
+++ unify_csv/unify_csv.py
@@ -2,6 +2,10 @@ import csv
 import pandas as pd
 import sys
 
+import tkinter as tk 
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.messagebox import showinfo
+
 #source_path='./test_source.csv'
 #dest_path='./test_destination.csv'
 
@@ -20,27 +24,54 @@ def csv_import(path):
         res=pd.concat(dataframes)
         return res
 
-def print_help():
+window = tk.Tk()
+window.title("Unify CSV")
+frame = tk.Frame(window,height = 200, width = 300)
+frame.pack()
+lb_file=tk.Label(frame, text="[no file chosen]")
+save_btn = tk.Button(frame, text="Save")
+
+def info():
     text='''
-    CSV Document Unifier
-        lazari@telekom
+CSV Document Unifier
+    lazari@telekom
 
     Purpose:
-        Unifies multiple .CSV configurations within a single .CSV file into a single dataset containing all fields from multiple datasets contained in a document
-    
+Unifies multiple .CSV configurations within a single .CSV file into a single dataset containing all fields from multiple datasets contained in a document
+
     Requirements:
-        First row in a dataset header MUST start with '$' (As seen in exported NOKIA .csv configurations)
-        Headers with less than 2 parameters will be ignored!
-        If overwrite is attempted (destination file exists) progra will throw an Error!
-
-    Usage (executed from cmd):
-        >unify_csv.exe ["source_file_path"] ["destination_file_path"]
+First row in a dataset header MUST start with '$' (As seen in exported NOKIA .csv configurations)
+Headers with less than 2 parameters will be ignored!
+If overwrite is attempted (destination file exists) program will throw an Error!
     '''
-    print(text)
+    showinfo(title="About", message=text)
 
-if(len(sys.argv)!=3):
-    print_help()
-    sys.exit()
-source_path=sys.argv[1]
-dest_path=sys.argv[2]
-csv_import(source_path).to_csv(path_or_buf=dest_path,index=False)
+def save(file):
+    if(file is None or file==''):
+        return
+    dest = asksaveasfilename(filetypes=[("CSV files", "*.csv")], defaultextension='csv')
+    if(dest is None or dest==''):
+        return
+    csv_import(file).to_csv(path_or_buf=dest,index=False)
+    showinfo(title="Success", message="File converted Succesfuly")
+
+def open_file():
+    file = askopenfilename(filetypes=[("CSV files", "*.csv")])
+    if(file!='' and file is not None):
+        lb_file['text'] = file
+        save_btn.configure(text="Save",command= lambda *args: save(file))
+        save_btn.grid(row = 3, column = 1, ipadx=10, ipady=10, padx=10, pady=10)
+    else:
+        save_btn.grid_forget()
+        lb_file['text'] = '[no file chosen]'
+
+save_btn['command']=save
+tk.Label(frame, text="Unify CSV").grid(row = 1, column = 1, ipadx=10, ipady=10, padx=10, pady=10)
+tk.Label(frame, text="Source file:").grid(row = 2, column = 0, ipadx=10, ipady=10, padx=10, pady=10)
+lb_file.grid(row = 2, column = 1, ipadx=10, ipady=10, padx=10, pady=10)
+tk.Button(frame, text="Open", command=open_file).grid(row = 2, column = 2, ipadx=10, ipady=10, padx=10, pady=10)
+
+tk.Button(frame, text="Info", command=info).grid(column=1, row=5,sticky ='s')
+
+window.geometry('800x400')
+window.mainloop()
