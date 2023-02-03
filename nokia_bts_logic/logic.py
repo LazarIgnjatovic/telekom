836917fd330866797755987d6db2552b9aff9c91
Lazar Ignjatovic <lazar.ignjatovic@elfak.rs>
2023-02-03 12:58:53 +0100

    logic done

--- nokia_bts_logic/logic.py
+++ nokia_bts_logic/logic.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-from openpyxl import Workbook
+import openpyxl as xl
 import copy
 import re
 
@@ -12,68 +12,133 @@ cell_ext = ['A','B','C','D']
 
 new_name = 'KVL89'
 new_label = 'KVL89_ZC_Studenica_GL'
-new_id ='4741'
 new_location = 'ZC_Studenica'
-
-input_path = 'C:\\Poso\\Nokia\\Stanice\\KVO89\\Configuration_kgl136_MUSTRA_modified_20230201-1446.xml'
-output_path = "test.xml"
-
 bcf_id = '3965'
 omusig_remote_addr = '172.21.7.28'
 
-address_space = [
-    ['10.206.149.58','10.206.149.57','1010'],
-    ['10.245.102.30','10.245.102.29','1011'],
-    ['10.244.102.30','10.244.102.29','1012'],
-    ['10.243.51.22','10.243.51.21','1013'],
-]
+lte_path = "C:\\Poso\\Nokia\\LTE_tabele\\LTE_podaci_01_02_2023.xlsx"
+ip_path = "C:\\Poso\\Nokia\\IP_tabele\\98NSN ALL IP i LTE  Tabela ver14.0.xlsm"
+input_path = 'C:\\Poso\\Nokia\\Stanice\\KVO89\\Configuration_kgl136_MUSTRA_modified_20230201-1446.xml'
+output_path = "test.xml"
 
-#from LTE table
-tac = '20411'
-LNcell_ids = {
-    'KVL89':['47411','47412','47413','47414'],
-    'KVJ89':['1901','1902','1903','1904'],
-    'KVO89':['47415','47416','47417','47418'],
-}
+global address_space
+global tac
+global new_id
+global LNcell_ids
+global rootSeqCodes
+global cell_ids
+global PHcell_ids
 
-rootSeqCodes = {
-    'KVL89':['300','310','320','390'],
-    'KVJ89':['300','310','320','390'],
-    'KVO89':['660','682','704','0'],
-}
-
-cell_ids = {
-    'KVL89':['1213697','1213698','1213699','1213700'],
-    'KVJ89':['1213787','1213788','1213789','1213790'],
-    'KVO89':['1213701','1213702','1213703','1213704'],
-}
-
-PHcell_ids = {
-    'KVL89':['30','31','32','39'],
-    'KVJ89':['30','31','32','39'],
-    'KVO89':['30','31','32','39'],
-}
+def log(message:str):
+    print(message+'...')
 
 def import_bts(file_path: str) -> ET.ElementTree:
+    log('Importing template')
     with open(file_path,'r') as f:
         d=f.read()
 
     data = ET.parse(file_path)
     return data
 
-def import_lte_table(path:str):
-    # TODO
-    #indeksiraj relevantna polja
+def import_lte_table(path:str, code:str):
+    log('Opening LTE table')
+    workbook = xl.load_workbook(filename=path, data_only=True)
+    sheet = workbook.active
+    log('Finding Relevant Rows')
+    loc_code = code[:2]
+    num_code = re.findall(r'\d+', code)[0]
+    relevant_rows = []
+    enb_col = 1 
+    tac_col = 0
+    lncell_cols=[]
+    root_cols=[]
+    trace_cols=[]
+    ph_cols=[]
+    for cell in sheet['A']:
+        if cell.value is not None and len(cell.value) > 2 and len(re.findall(r'\d+', cell.value)) > 0:
+            cell_loc_code = cell.value[:2]
+            cell_num_code = re.findall(r'\d+', cell.value)[0]
+            if cell_loc_code == loc_code and cell_num_code == num_code:
+                relevant_rows.append(cell.row)
+    for cell in sheet[1]:
+        if cell.value is not None:
+            if 'LNcell ID ' in cell.value:
+                lncell_cols.append(cell.column-1)
+            elif 'CellId' in cell.value:
+                trace_cols.append(cell.column-1)
+            elif 'TAC' == cell.value:
+                tac_col = cell.column-1
+            elif len(cell.value) == 4 and 'PCI' in cell.value:
+                ph_cols.append(cell.column-1)
+            elif 'rachRootSequence' in cell.value:
+                root_cols.append(cell.column-1)
 
-    #iteracija kroz polja koja sadrze kod i prikupljanje param [kgl, kgo, ...]
+    log("Collecting data")
+    global tac
+    global new_id
+    global LNcell_ids
+    global rootSeqCodes
+    global cell_ids
+    global PHcell_ids
+    tac = ''
+    new_id = ''
+    LNcell_ids = {}
+    rootSeqCodes = {}
+    cell_ids = {}
+    PHcell_ids = {}
 
-    return
+    for row in relevant_rows:
+        tac = str(sheet[row][tac_col].value)
+        new_id = str(sheet[row][enb_col].value)
 
-def import_ip_table(path:str):
-    # TODO
+        c = sheet[row][0].value
+        LNcell_ids[c] = []
+        rootSeqCodes[c] = []
+        cell_ids[c] = []
+        PHcell_ids[c] = []
 
-    # set() <= polja koja imaju kod
-    # iteriraj kroz polja i skupi ip, gateway i vlan
+        for col in lncell_cols:
+            if sheet[row][col].value is not None:
+                LNcell_ids[c].append(str(sheet[row][col].value))
+        for col in root_cols:
+            if sheet[row][col].value is not None:
+                rootSeqCodes[c].append(str(sheet[row][col].value))
+        for col in trace_cols:
+            if sheet[row][col].value is not None:
+                cell_ids[c].append(str(sheet[row][col].value))
+        for col in ph_cols:
+            if sheet[row][col].value is not None:
+                PHcell_ids[c].append(str(sheet[row][col].value))
+    return 
+
+def import_ip_table(path:str, code:str):
+    log("Loading IP table")
+    workbook = xl.load_workbook(filename=path, data_only=True, read_only=True)
+    sheet = workbook.active
+
+    relevant_rows = []
+    global address_space 
+    address_space = []
+    ip_int_column = 34
+    loc_code = code[:2]
+    num_code = re.findall(r'\d+', code)[0]
+    log('Searching for relevant fields')
+    for row in sheet.rows:
+        cell = row[ip_int_column]
+        if cell.value is not None and type(cell.value) is str and len(cell.value) > 2 and len(re.findall(r'\d+', cell.value)) > 0:
+            cell_loc_code = cell.value[:2]
+            cell_num_code = re.findall(r'\d+', cell.value)[0]
+            if cell_loc_code == loc_code and cell_num_code == num_code:
+                relevant_rows.append(cell.row)
+
+    log('Constructing address space')
+    for row in relevant_rows:
+        ip = sheet[row][ip_int_column+1].value.split('/')[0]
+        vlan = sheet[row][ip_int_column+2].value
+        octets = ip.split('.')
+        octets[-1] = str(int(octets[-1])-1)
+        gateway = '.'.join(octets)
+        address_space.append([ip,gateway,vlan])
 
     return
 
@@ -97,8 +162,8 @@ def get_cell_map(orig: ET.ElementTree):
             cell_map[to_replace]=new_val
     return cell_map
 
-
 def MRBTS_change(orig: ET.ElementTree, new_id:str, label:str, name:str, location:str):
+    log('General info update')
     changed = copy.deepcopy(orig)
     root=changed.getroot()
 
@@ -120,6 +185,8 @@ def MRBTS_change(orig: ET.ElementTree, new_id:str, label:str, name:str, location
     old_code = search[0].text
     for elem in search:
         elem.text=name
+
+    log('Cell Name and Id update')
 
     new_loc_code = new_name[:2]
     old_loc_code = old_code[:2]
@@ -162,6 +229,7 @@ def MRBTS_change(orig: ET.ElementTree, new_id:str, label:str, name:str, location
     return changed
 
 def cell_id_change(orig: ET.ElementTree):
+    log('TraceId update')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
 
@@ -181,6 +249,7 @@ def cell_id_change(orig: ET.ElementTree):
     return changed
 
 def phy_cell_id_change(orig: ET.ElementTree):
+    log('PhCellId update')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
 
@@ -196,6 +265,7 @@ def phy_cell_id_change(orig: ET.ElementTree):
     return changed
 
 def root_seq_change(orig: ET.ElementTree):
+    log('RACH update')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
     
@@ -213,6 +283,7 @@ def root_seq_change(orig: ET.ElementTree):
     return changed
 
 def tac_change(orig: ET.ElementTree):
+    log('Tac update')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
     search = root.findall('.//*[@name="tac"]')
@@ -221,6 +292,7 @@ def tac_change(orig: ET.ElementTree):
     return changed
 
 def bcf_change(orig:ET.ElementTree):
+    log('Changing BCF data')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
 
@@ -233,6 +305,7 @@ def bcf_change(orig:ET.ElementTree):
     return changed
 
 def address_change(orig:ET.ElementTree):
+    log('Changig IP address space')
     changed = copy.deepcopy(orig)
     root = changed.getroot()
 
@@ -270,8 +343,9 @@ def address_change(orig:ET.ElementTree):
 
     return changed
 
-
 data = import_bts(file_path=input_path)
+import_lte_table(lte_path,new_name)
+import_ip_table(ip_path,new_name)
 changed = MRBTS_change(data,new_id,new_label,new_name,new_location)
 changed = tac_change(changed)
 changed = root_seq_change(changed)
